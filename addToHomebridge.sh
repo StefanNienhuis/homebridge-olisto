5dce27713e897d3828ac7542c45a4c5ea2be1963
Alexander Sterk <ajhsterk@gmail.com>
2019-01-27 16:30:35 +0100

    Script to add new device easily

--- addToHomebridge.sh
+++ addToHomebridge.sh
@@ -7,5 +7,13 @@ if [ -z "$1" ]
 	exit 1
 fi
 
-jq --arg argo "$1" '.platforms[] | select(.platform=="Olisto").triggs += [{"name":$argo,"type":"switch","connecturl":"'$OLISTO_URL'"}]' $CONFIG_LOCATION
-
+new=$(jq --arg argo "$1" '.platforms = [.platforms[] | select(.platform=="Olisto").triggs += [{"name":$argo,"type":"switch","connecturl":"'$OLISTO_URL'"}]]' $CONFIG_LOCATION)
+jq . <<<  "$new"
+read -p "Save this new config file? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    cp $CONFIG_LOCATION $CONFIG_LOCATION.bak
+    echo "$new" > $CONFIG_LOCATION
+    service homebridge restart
+fi
